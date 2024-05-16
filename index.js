a575b1173bdfa10204b34a538a8a134758a795a1
Zandile Pretty Ngema <zandilepretty01@gmail.com>
2024-05-16 19:10:12 +0200

    Writing some basic javascript

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 let firstCard = 5;
 let secondCard = 7;
-let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let sum = firstCard + secondCard + 9;
 console.log(sum);
 
-let age = 22;
-
-
+if (sum <= 20) {
+  console.log("Do you wat to draw a new card?");
+} else if (sum === 21) {
+  console.log("Hooray!! You have a Blackjack");
+  hasBlackJack = true;
+} else {
+  console.log("You are out of the game");
+  isAlive = false;
+}
+console.log(isAlive);
